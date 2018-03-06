b3c58128ed8a2287fee139bd5b84e508ad41e60a
Guest User <Guest@Epicodus-7E.local>
2018-03-05 16:36:25 -0800

    add scrape files

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require("bundler/setup")
 require("pry")
+require("open-uri")
 # require("./lib/meetup.rb")
 Bundler.require(:default)
 
@@ -61,4 +62,24 @@ get('/') do
   end
   @meetups = Meetup.all
   erb(:meetup_test)
+end
+
+get("/scrape") do
+  @link = "https://www.indeed.com/jobs?q=jr+developer&l=Portland%2C+OR"
+  @page = Nokogiri::HTML(open("#{@link}"))
+
+  #amount of jobs to get page amounts
+  @amount_of_jobs = @page.css('#searchCount').to_s
+  @amount_of_jobs =~ /Page 1 of (.*?) jobs/
+  @amount_of_jobs = $1.gsub(/\,/, '')
+  @amount_of_pages = @amount_of_jobs.to_i/14
+
+  @page_stuff = []
+  @amount_of_pages.times do |i|
+    @page = Nokogiri::HTML(open("#{@link}&start=#{i*10}"))
+    @page_stuff.push(@page.css('.row'))
+    puts i
+  end
+
+  erb(:scrape)
 end
