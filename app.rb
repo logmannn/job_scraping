f1fe8f6d8900ca56b06cbba55e82a42a47fc5bdd
russell hofvendahl <rhofvendahl@gmail.com>
2018-03-08 11:37:04 -0800

    add popular topics

--- app.rb
+++ app.rb
@@ -22,11 +22,11 @@ get('/test') do
 
   @members["results"].each do |member|
     member_attributes = {}
-    member_attributes["name"] = member["name"]
-    member_attributes["city"] = member["city"]
-    member_attributes["state"] = member["state"]
-    member_attributes["country"] = member["country"]
-    # member_attributes["linkedin_user_id"] = member["description"]
+    member_attributes[:name] = member["name"]
+    member_attributes[:city] = member["city"]
+    member_attributes[:state] = member["state"]
+    member_attributes[:country] = member["country"]
+    # member_attributes[:linkedin_user_id] = member["description"]
     MeetupMember.create(member_attributes)
   end
   @meetup_members = MeetupMember.all
@@ -62,31 +62,32 @@ post('/') do
 
   @events["results"].each do |event|
     meetup_attributes = {}
-    meetup_attributes["utc_offset"] = event["utc_offset"]
-    meetup_attributes["time"] = event["time"]
-    meetup_attributes["event_url"] = event["event_url"]
-    meetup_attributes["name"] = event["name"]
-    meetup_attributes["description"] = event["description"]
-    meetup_attributes["yes_rsvp_count"] = event["yes_rsvp_count"]
-    meetup_attributes["status"] = event["status"]
+    meetup_attributes[:utc_offset] = event["utc_offset"]
+    meetup_attributes[:time] = event["time"]
+    meetup_attributes[:event_url] = event["event_url"]
+    meetup_attributes[:name] = event["name"]
+    meetup_attributes[:description] = event["description"]
+    meetup_attributes[:yes_rsvp_count] = event["yes_rsvp_count"]
+    meetup_attributes[:status] = event["status"]
 
     if event["venue"]
-      meetup_attributes["venue_name"] = event["venue"]["name"]
-      meetup_attributes["venue_city"] = event["venue"]["city"]
-      meetup_attributes["venue_state"] = event["venue"]["state"]
-      meetup_attributes["venue_zip"] = event["venue"]["zip"]
-      meetup_attributes["venue_country"] = event["venue"]["country"]
-      meetup_attributes["venue_address_1"] = event["venue"]["address_1"]
+      meetup_attributes[:venue_name] = event["venue"]["name"]
+      meetup_attributes[:venue_city] = event["venue"]["city"]
+      meetup_attributes[:venue_state] = event["venue"]["state"]
+      meetup_attributes[:venue_zip] = event["venue"]["zip"]
+      meetup_attributes[:venue_country] = event["venue"]["country"]
+      meetup_attributes[:venue_address_1] = event["venue"]["address_1"]
     end
 
     if event["group"]
-      meetup_group_attributes = {}
-      meetup_group_attributes["name"] = event["group"]["name"]
-      meetup_group_attributes["group_url"] = "https://www.meetup.com/#{event["group"]["urlname"]}"
-
-      meetup_group = MeetupGroup.find_or_create_by(meetup_group_attributes)
-
-      meetup_attributes["group_id"] = meetup_group.id
+      # meetup_group_attributes = {}
+      # meetup_group_attributes[:name] = event["group"]["name"]
+      # meetup_group_attributes[:group_url] = "https://www.meetup.com/#{event["group"]["urlname"]}"
+      #
+      # meetup_group = MeetupGroup.find_or_create_by(meetup_group_attributes)
+      #
+      # meetup_attributes[:group_id] = meetup_group.id
+      meetup_attributes[:group_id] = event["group"]["id"]
     end
 
     Meetup.create(meetup_attributes)
@@ -141,6 +142,23 @@ end
 
 get('/meetups/:meetup_id') do
   @meetup = Meetup.find(params[:meetup_id])
+  member_params = { group_id: @meetup.group_id,
+    format: 'json',
+    page: '50'}
+  meetup_group_members = meetup_api.members(member_params)
+  @topics = {}
+  meetup_group_members["results"].each do |member|
+    member["topics"].each do |topic|
+      topic_name = topic["name"]
+      if @topics[topic_name]
+        @topics[topic_name] += 1
+      else
+        @topics[topic_name] = 1
+      end
+    end
+  end
+  @topics
+  # @meetup_members = MeetupMember.all
   erb(:meetup_detail)
 end
 
